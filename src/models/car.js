6f47f16b443ca67f66ea9590710d807665f84582
Betul <betulkoru06@gmail.com>
2024-04-01 22:06:48 +0300

    car model

--- src/models/car.js
+++ src/models/car.js
@@ -29,3 +29,70 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // Car Model:
+
+const CarSchema = new mongoose.Schema({
+
+    plateNumber: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+    },
+
+    brand: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    model: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    year: {
+        type: Number,
+        min: 1950,
+        max: new Date().getFullYear(),
+        required: true,
+    },
+    
+    isAutomatic: {
+        type: Boolean,
+        default: false,
+    },
+
+    pricePerDay: {
+        type: Number,
+        required: true,
+    },
+
+    // images: [],
+    images: {
+        type: Array,
+        default: [],
+    },
+
+    isAvailable: {
+        type: Boolean,
+        default: true,
+    },
+
+    createdId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    updatedId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+} , {
+    collection: 'cars',
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Car', CarSchema);
