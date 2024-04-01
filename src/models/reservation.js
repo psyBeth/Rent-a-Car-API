68f502d22d642b91c4bfd3a39b2c0267ccec9950
Betul <betulkoru06@gmail.com>
2024-04-01 22:27:38 +0300

    reservation model

--- src/models/reservation.js
+++ src/models/reservation.js
@@ -15,4 +15,51 @@ const { mongoose } = require('../configs/dbConnection')
     "endDate": "2023-10-20"
 }
 /* ------------------------------------------------------- */
-// Reservation Model:
+
+const ReservationSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    carId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    startDate: {
+        type: Date,
+        required: true,
+    },
+
+    endDate: {
+        type: Date,
+        required: true,
+    },
+
+    amount: {
+        type: Number,
+        required: true,
+    },
+
+    createdId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    updatedId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+} , {
+    collection: 'reservations',
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Reservation', ReservationSchema);
