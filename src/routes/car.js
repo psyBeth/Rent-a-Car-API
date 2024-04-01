c1b5a83f23b15a7561855cadde8c4e29c2c7ebb7
Betul <betulkoru06@gmail.com>
2024-04-01 22:41:54 +0300

    routes

--- src/routes/car.js
+++ src/routes/car.js
@@ -1,12 +1,21 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+/* ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
-// routes/car:
 
+const car = require('../controllers/car')
 
+// URL: /cars
+
+router.route('/')
+    .get(car.list)
+    .post(car.create)
+
+router.route('/:id')
+    .get(car.read)
+    .put(car.update)
+    .patch(car.update)
+    .delete(car.delete)
 
 /* ------------------------------------------------------- */
- module.exports = router;
+module.exports = router
