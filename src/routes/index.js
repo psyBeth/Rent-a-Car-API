740a6683916caa19320c7a35bec87e1b8d165eba
Betul <betulkoru06@gmail.com>
2024-04-01 21:41:35 +0300

    jwt installed index router built

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const router = require('express').Router();
+
+// URL: /
+
+// auth:
+router.use('/auth', require('./auth'))
+// user:
+router.use('/users', require('./user'))
+// token:
+router.use('/tokens', require('./token'))
+
+// car:
+router.use('/cars', require('./car'))
+
+// reservation:
+router.use('/reservations', require('./reservation'))
+
+// document:
+router.use('/documents', require('./document'))
+
+/* ------------------------------------------------------- */
+
+module.exports = router
