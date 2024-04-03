efa3c7d37dea41df437fac67ed0e19ad3a86c0c4
Betul <betulkoru06@gmail.com>
2024-04-03 21:20:04 +0300

    isAvailabke to example data comments

--- src/models/car.js
+++ src/models/car.js
@@ -8,7 +8,8 @@ const { mongoose } = require('../configs/dbConnection')
     "model": "Focus",
     "year": 2020,
     "isAutomatic": true,
-    "pricePerDay": 249.99
+    "pricePerDay": 249.99,
+    "isAvailable": true,
 }
 {
     "plateNumber": "34ABC234",
@@ -16,7 +17,8 @@ const { mongoose } = require('../configs/dbConnection')
     "model": "Megane",
     "year": 2022,
     "isAutomatic": false,
-    "pricePerDay": 199.99
+    "pricePerDay": 199.99,
+    "isAvailable": true,
 }
 {
     "plateNumber": "34ABC345",
@@ -25,7 +27,8 @@ const { mongoose } = require('../configs/dbConnection')
     "year": 2021,
     "isAutomatic": false,
     "pricePerDay": 189.99,
-    "isPublish": false
+    "isPublish": false,
+    "isAvailable": true,
 }
 /* ------------------------------------------------------- */
 // Car Model:
